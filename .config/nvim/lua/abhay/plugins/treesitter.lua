0c863fdbaad725ea87f13e6909b219f85b2a9668
falcon71181 <abhaythakur71181@gmail.com>
2024-06-28 15:45:40 +0530

    fix: autotag treesitter fixed

--- .config/nvim/lua/abhay/plugins/treesitter.lua
+++ .config/nvim/lua/abhay/plugins/treesitter.lua
@@ -34,14 +34,24 @@ return {
         enable = true,
         additional_vim_regex_highlighting = false,
       },
-      autotag = {
-        enable = true,
-      },
-      autopairs = {
-        enable = true,
-      },
       rainbow = {
         enable = true,
+      },
+    }
+    require("nvim-ts-autotag").setup {
+      opts = {
+        -- Defaults
+        enable_close = true, -- Auto close tags
+        enable_rename = true, -- Auto rename pairs of tags
+        enable_close_on_slash = false, -- Auto close on trailing </
+      },
+      -- Also override individual filetype configs, these take priority.
+      -- Empty by default, useful if one of the "opts" global settings
+      -- doesn't work well in a specific filetype
+      per_filetype = {
+        ["html"] = {
+          enable_close = false,
+        },
       },
     }
   end,
